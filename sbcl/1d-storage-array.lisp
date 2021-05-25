fb388629599bb8b75881262f0e83ef66c2cf4a01
digikar99 <shubhamkarayare@gmail.com>
2021-05-25 12:04:19 +0530

    [remove] leave sbcl specific parts to sb-simd

--- sbcl/1d-storage-array.lisp
+++
@@ -1,8 +0,0 @@
-(in-package :numericals.sbcl)
-
-(defun 1d-storage-array (array)
-  (declare (optimize (speed 3))
-           (type array array))
-  (if (typep array 'simple-array)
-      (values (array-storage-vector array) 0)
-      (array-displacement array)))
