822fd2d2a5a0c76b7faabcd54ca3526bab3d024a
digikar99 <shubhamkarayare@gmail.com>
2021-10-08 11:22:33 +0530

    [add] dn: add *inline-with-multithreading* to try reduce code-bloat
    This has been a failure so far; the code generated *with*
    multithreading is shorter than the code generated without. It could be
    that SBCL and LPARALLEL are doing something, since the foreign
    function is not even visible after inlining.

--- dense-numericals-src/lparallel.lisp
+++ dense-numericals-src/lparallel.lisp
@@ -1,5 +1,10 @@
 (in-package :dense-numericals.impl)
 
+(defparameter dn:*inline-with-multithreading* nil
+  "Inlining is usually necessary for smaller arrays; for such arrays multithreading
+becomes unnecessary. If this parameter is non-NIL, code using multithreading
+would be emitted; otherwise, the code would be skipped.")
+
 (defparameter dn:*multithreaded-threshold* 80000
   "The lower bound of the array size beyond which LPARALLEL is used for distributing
 operations across multiple threads.
@@ -16,48 +21,53 @@ NOTE: It is not defined if this bound is inclusive or exclusive.")
          (vars     (if simple-p
                        (rest vars)
                        vars))
-         (out      (lastcar vars)))
-    `(block thresholded-multithreading
-       (if (< (the size ,threshold-measure)
-              dn:*multithreaded-threshold*)
-           (progn ,@body)
-           ;; FIXME: Can the multiple BODY here be reduced?
-           (progn
-             (let* ((worker-count  (lparallel:kernel-worker-count)))
-               (declare (type dense-arrays::size worker-count))
-               (multiple-value-bind (long-enough-axis axis/work-size)
-                   ,(if simple-p
-                        `(values 0 (array-total-size (the array ,out)))
-                        `(loop :for i :of-type size :from 0
-                               :for d :of-type size :in (narray-dimensions ,out)
-                               :if (>= d worker-count)
-                                 :do (return (values i d))
-                               :finally
-                                  (return-from thresholded-multithreading
-                                    (progn ,@body))))
-                 (declare (type size long-enough-axis axis/work-size))
-                 (let ((max-work-size (ceiling axis/work-size worker-count)))
-                   (declare (type size max-work-size))
-                   (lparallel:pdotimes (thread-idx worker-count)
-                     (declare (type dense-arrays::size thread-idx))
-                     (let* ((index (nconc (if (zerop long-enough-axis)
-                                              ()
-                                              (make-list long-enough-axis))
-                                          (list (list (the-size (* thread-idx max-work-size))
-                                                      :end (if (= thread-idx
-                                                                  (1- worker-count))
-                                                               axis/work-size
-                                                               (the-size (* (1+ thread-idx)
-                                                                            max-work-size)))))))
-                            ,@(loop :for var :in vars
-                                    :collect `(,var
-                                               (apply #'aref
-                                                      ,(if simple-p
-                                                           `(reshape ,var
-                                                                     (array-total-size
-                                                                      (the array ,var))
-                                                                     :view t)
-                                                           var)
-                                                      index))))
-                       (declare (type array ,@vars))
-                       ,@body))))))))))
+         (out      (lastcar vars))
+         (body-sym (gensym "BODY")))
+    (if (and polymorphic-functions:*compiler-macro-expanding-p*
+             (not dn:*inline-with-multithreading*))
+        `(progn ,@body)
+        `(block thresholded-multithreading
+           (flet ((,body-sym ,vars
+                    ,@body))
+             (if (< (the size ,threshold-measure)
+                    dn:*multithreaded-threshold*)
+                 (,body-sym ,@vars)
+                 (progn
+                   (let* ((worker-count  (lparallel:kernel-worker-count)))
+                     (declare (type dense-arrays::size worker-count))
+                     (multiple-value-bind (long-enough-axis axis/work-size)
+                         ,(if simple-p
+                              `(values 0 (array-total-size (the array ,out)))
+                              `(loop :for i :of-type size :from 0
+                                     :for d :of-type size :in (narray-dimensions ,out)
+                                     :if (>= d worker-count)
+                                       :do (return (values i d))
+                                     :finally
+                                        (return-from thresholded-multithreading
+                                          (,body-sym ,@vars))))
+                       (declare (type size long-enough-axis axis/work-size))
+                       (let ((max-work-size (ceiling axis/work-size worker-count)))
+                         (declare (type size max-work-size))
+                         (lparallel:pdotimes (thread-idx worker-count)
+                           (declare (type dense-arrays::size thread-idx))
+                           (let* ((index (nconc (if (zerop long-enough-axis)
+                                                    ()
+                                                    (make-list long-enough-axis))
+                                                (list (list (the-size (* thread-idx max-work-size))
+                                                            :end (if (= thread-idx
+                                                                        (1- worker-count))
+                                                                     axis/work-size
+                                                                     (the-size (* (1+ thread-idx)
+                                                                                  max-work-size)))))))
+                                  ,@(loop :for var :in vars
+                                          :collect `(,var
+                                                     (apply #'aref
+                                                            ,(if simple-p
+                                                                 `(reshape ,var
+                                                                           (array-total-size
+                                                                            (the array ,var))
+                                                                           :view t)
+                                                                 var)
+                                                            index))))
+                             (declare (type array ,@vars))
+                             (,body-sym ,@vars)))))))))))))
