b25e6d60dbbb31a28e19107f60c087d5b88e82ba
digikar99 <shubhamkarayare@gmail.com>
2021-10-12 14:39:39 +0530

    [add] dn: avoid using dot for sum, and thus speed things up

--- dense-numericals-src/sum.lisp
+++ dense-numericals-src/sum.lisp
@@ -11,38 +11,37 @@
   (dn:sum (asarray x) :axes axes :out out))
 
 
-(macrolet ((def (type c-fn type-size ctype)
+(macrolet ((def (type c-fn type-size)
 
              `(progn
                 (defpolymorph dn:sum ((x (array ,type))
                                       &key ((axes null)) ((out null)))
-                    ,type
+                    (values ,type &optional)
                   (declare (ignore axes out))
-                  (let ((sum 0.0f0))
-                    (cffi:with-foreign-pointer (ones ,type-size)
-                      (setf (cffi:mem-aref ones ,ctype)
-                            (coerce 1 ',type))
-                      (ptr-iterate-but-inner n ((ptr-x ,type-size inc-x x))
-                        (incf sum
-                              (,c-fn n
-                                     ptr-x 1
-                                     ones 0))))
-                    sum))
-
+                  (let ((sum ,(coerce 0 type)))
+                    (declare (type real sum))
+                    (ptr-iterate-but-inner n ((ptr-x ,type-size inc-x x))
+                      (incf sum (,c-fn n ptr-x inc-x)))
+                    (the ,type (trivial-coerce:coerce
+                                (trivial-coerce:coerce sum 'integer)
+                                ',type))))
                 (defpolymorph dn:sum ((x (array ,type 1))
                                       &key ((axes (eql 0))) ((out null)))
-                    ,type
+                    (values ,type &optional)
                   (declare (ignore axes out))
-                  (cffi:with-foreign-pointer (ones ,type-size)
-                    (setf (cffi:mem-aref ones ,ctype)
-                          (coerce 1 ',type))
-                    (cffi:with-pointer-to-vector-data (ptr-x (array-storage x))
-                      (,c-fn (array-total-size x)
-                             ptr-x 1
-                             ones 0))))
-
+                  (let ((sum
+                          (with-pointers-to-vectors-data ((ptr-x (array-storage x)))
+                            (let ((inc-x (array-stride x 0)))
+                              (,c-fn (array-total-size x)
+                                     ptr-x inc-x)))))
+                    (trivial-coerce:coerce sum ',type)))
                 (defpolymorph dn:sum ((x (array ,type))
-                                      &key ((axes integer)) ((out (array ,type))))
+                                      &key ((axes integer))
+                                      ((out (array ,type))
+                                       (zeros (let ((dim (narray-dimensions x)))
+                                                (append (subseq dim 0 axes)
+                                                        (nthcdr (+ 1 axes) dim)))
+                                              :type ',type)))
                     (array ,type)
                   ;; We obtain a slice of X into COPY-X
                   ;; This slice has a rank one less than X
@@ -61,43 +60,72 @@
                                     stride)))
                   out))))
 
-  (def single-float cblas:sdot 4 :float)
-  (def double-float cblas:ddot 8 :double))
-
-
+  (def single-float     bmas:ssum 4)
+  (def double-float     bmas:dsum 8)
+  (def (signed-byte 64) bmas:i64sum 8)
+  (def (signed-byte 32) bmas:i32sum 4)
+  (def (signed-byte 16) bmas:i16sum 2)
+  (def (signed-byte 08) bmas:i8sum  1)
+  (def (unsigned-byte 64) bmas:i64sum 8)
+  (def (unsigned-byte 32) bmas:i32sum 4)
+  (def (unsigned-byte 16) bmas:i16sum 2)
+  (def (unsigned-byte 08) bmas:i8sum  1))
 
 (5am:def-test dn:sum ()
-  (loop :for *array-element-type* :in '(single-float double-float)
-        :do
-           (5am:is (= 06 (dn:sum (asarray '(1 2 3)))))
-           (5am:is (= 21 (dn:sum (asarray '((1 2 3)
-                                            (4 5 6))))))
-           (5am:is (array= (asarray '(5 7 9))
-                           (dn:sum (asarray '((1 2 3)
-                                              (4 5 6)))
-                                   :axes 0
-                                   :out (zeros 3))))
-           (5am:is (array= (asarray '(6 15))
-                           (dn:sum (asarray '((1 2 3)
-                                              (4 5 6)))
-                                   :axes 1
-                                   :out (zeros 2))))
-           (5am:is (array= (asarray '((5 7 9)
-                                      (7 9 11)))
-                           (dn:sum (asarray '(((1 2 3)
-                                               (4 5 6))
-                                              ((7 8 9)
-                                               (0 1 2))))
-                                   :axes 1
-                                   :out (zeros 2 3))))
-           (5am:is (array= (asarray '((6 15)
-                                      (24 3)))
-                           (dn:sum (asarray '(((1 2 3)
-                                               (4 5 6))
-                                              ((7 8 9)
-                                               (0 1 2))))
-                                   :axes 2
-                                   :out (zeros 2 2))))))
+  (flet ((float-close-p (x y)
+           (or (= x y)
+               (progn
+                 ;; (print (list x y))
+                 (< (/ (abs (- x y))
+                       (+ (abs x) (abs y)))
+                    0.01)))))
+    (loop :for *array-element-type* :in `(single-float
+                                          double-float
+                                          (signed-byte 64)
+                                          (signed-byte 32)
+                                          (signed-byte 16)
+                                          (signed-byte 08)
+                                          (unsigned-byte 64)
+                                          (unsigned-byte 32)
+                                          (unsigned-byte 16)
+                                          (unsigned-byte 08))
+          :do
+             (5am:is (= 06 (dn:sum (asarray '(1 2 3)))))
+             (5am:is (= 21 (dn:sum (asarray '((1 2 3)
+                                              (4 5 6))))))
+             (5am:is (array= (asarray '(5 7 9))
+                             (dn:sum (asarray '((1 2 3)
+                                                (4 5 6)))
+                                     :axes 0
+                                     :out (zeros 3))))
+             (5am:is (array= (asarray '(6 15))
+                             (dn:sum (asarray '((1 2 3)
+                                                (4 5 6)))
+                                     :axes 1
+                                     :out (zeros 2))))
+             (5am:is (array= (asarray '((5 7 9)
+                                        (7 9 11)))
+                             (dn:sum (asarray '(((1 2 3)
+                                                 (4 5 6))
+                                                ((7 8 9)
+                                                 (0 1 2))))
+                                     :axes 1
+                                     :out (zeros 2 3))))
+             (5am:is (array= (asarray '((6 15)
+                                        (24 3)))
+                             (dn:sum (asarray '(((1 2 3)
+                                                 (4 5 6))
+                                                ((7 8 9)
+                                                 (0 1 2))))
+                                     :axes 2
+                                     :out (zeros 2 2))))
+
+             (let ((array (dn:rand 100)))
+               (5am:is (float-close-p (dn:sum array)
+                                      (let ((sum 0))
+                                        (do-arrays ((x array))
+                                          (incf sum x))
+                                        sum)))))))
 
 ;; ;;; The perf diff between simple and non-simple versions is less than 5%
 ;; ;;; for (RAND 1000 1000); may be we could delete this (?)
