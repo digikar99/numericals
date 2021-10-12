f1472c2b5538e531a4361528fda68e82844727e5
digikar99 <shubhamkarayare@gmail.com>
2021-10-12 14:42:32 +0530

    [fix] misc one-arg-fn fixes for inlining

--- dense-numericals-src/misc.lisp
+++ dense-numericals-src/misc.lisp
@@ -6,12 +6,3 @@
         (nth axis shape)
         shape)))
 
-;; arbitrary arrays
-(defpolymorph (one-arg-fn :inline t) ((name symbol) (x array) &key ((out array))) array
-  ;; FIXME: We are assuming OUT is either single-float or double-float
-  (if (type= (array-element-type x)
-             (array-element-type out))
-      (one-arg-fn name x :out out)
-      (one-arg-fn name (dn:copy x :out out) :out out))
-  out)
-
